9e2be07f79259d0cda7c3b58c5384299a00912d5
AndyJudkis <ajudkis@yahoo.com>
2018-08-21 22:58:38 -0400

    Most of the required features are implemented

--- application.py
+++ application.py
@@ -14,27 +14,34 @@ channels = { "Welcome!":  [{"text":"Welcome to FlackChat!", "from":"admin", "tim
 def index():
     return render_template("index.html")
 
+# client sends 'join' when it starts up -- joins with channel name saved in browser, defaults to Welcome!
+# server responds with 'welcome', with a list of channels, and a list of the messages for the channel that the
+# client joined, sent to joining client only
 @socketio.on("join")
-def join(data): 
+def join(joinChan):
     # What if client joins with non-existent channel name? (as in case
     # when server restarts but browser remembers old name) My solution is to make a new
     # channel, and tell other clients that there's a new channel
     ret = []
-    if data not in channels:
-        channels[data] = []
-        emit("channel added", data, broadcast=True)
+    if joinChan not in channels:
+        channels[joinChan] = []
+        emit("channel added", joinChan, broadcast=True)
     
     # make list of channels
     for nxt in channels:
         ret.append(nxt);
     # add messages for the channel the user joined on
-    ret.append(channels[data])
-    print(f"client joined channel {data}")
-    print(f"returning: {ret}")
+    ret.append(channels[joinChan])
+    print(f"all channel info {channels}")
+    print(f"client joined channel {joinChan}")
+    print(f"sending {ret} to client")
     emit("welcome", ret, broadcast=False)    
         
 
 @socketio.on("newChannel")
+# client sends 'newChannel' when it creates a channel.  This routine is also used when server needs
+# to recreate a channel that a client thinks is there.  Broadcasts 'channel added' with new
+# channel name to all clients
 def newChannel(data):
     newName = data["channelName"]
     print(f"newChannel: {newName}")
@@ -45,29 +52,43 @@ def newChannel(data):
     	channels[newName] = []
     	emit("channel added", newName, broadcast=True)
 
+# client sends 'setChannel' when channel is set
+# server responds with 'channel changed', and the current messages on that channel, only to the client
+# that changed channels
 @socketio.on("setChannel")
 def setChannel(data):
     newName = data["channelName"]
     print(f"setChannel: {newName}")
-    print(channels[newName])
     if newName not in channels:
-        # TODO - return error if channel doesn't exist
-        # or better to just create it?
-    	print(f"name {newName} doesn't exist")
+        # create channel if it doesn't exist -- for instance, if server restarted but client still in chat
+        print(f"name {newName} doesn't exist")
+        newChannel(data)
     else:
-    	emit("channel changed", channels[newName], broadcast=False)
-        
+        print(f"sending {channels[newName]} to client")
+        emit("channel changed", channels[newName], broadcast=False)
+
+# client sends 'sendMsg' with channel, text, sender name, server adds timestamp
+# and broadcasts 'new msg' to all clients.  It's up to the client to discard messages on channels that
+# it isn't interested in
 @socketio.on("sendMsg")
 def sendMsg(data):
+    print(f"current state of channels: {channels}")
+    print(f"client sent us data: {data}")
     thisChannel = data["chan"]
     thisMsg = data["text"]
+ 
+    if thisChannel not in channels:
+        # create channel if it doesn't exist -- for instance, if server restarted but client still in chat
+        print(f"name {thisChannel} doesn't exist")
+        newChannel(thisChannel)
+    channels[thisChannel].append({"text": thisMsg, "from" : data["from"], "date" : ""})
+
     # TODO: add time stamp to msg
     # TODO: limit to 100 msgs
-    channels[thisChannel].append({"text": thisMsg, "from" : data["from"], "date" : ""})
-    print(channels[thisChannel])
     while len(channels[thisChannel]) > 5:
         #print("exceeded max # of messages per channel, deleting " + channels[thisChannel][0])
         channels[thisChannel] = channels[thisChannel][1:]
+    print(f"about to broadcast, new state of channels: {channels}")
     emit("new msg", data, broadcast=True)
     
 
--- static/index.js
+++ static/index.js
@@ -1,3 +1,10 @@
+// TODO
+// disable text entry on chat scroll
+// add timestamp
+// show username, timestamp with messages
+// attach file to msg (or something? . . . )
+// make it look good
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Connect to websocket
@@ -15,7 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#displayName').value = displayName;
         
         enableSendButton();
-        enableNewChannelButton();
 
         document.querySelector('#displayName').onkeyup = () => {
             localStorage.setItem('displayName', document.querySelector('#displayName').value);
@@ -26,61 +32,64 @@ document.addEventListener('DOMContentLoaded', () => {
             enableSendButton();
         };
 
-        document.querySelector('#newChannelName').onkeyup = () => {
-            enableNewChannelButton();
-        };
-
         document.querySelector('#newChannelButton').onclick = () => {
-                const channelName = document.querySelector('#newChannelName').value;
-                console.log(`new channel: ${channelName}`);
-                socket.emit('newChannel', {'channelName': channelName});
+            const newChan = prompt("New Channel Name?");
+            if (newChan) {
+                let currOptions = getChannelList(document.querySelector("#changeChannel"));
+
+                if (!currOptions.includes(newChan)) {
+                    socket.emit('newChannel', {'channelName': newChan});
+                    currChannel = newChan;
+                    localStorage.setItem("currChannel", newChan);
+                    socket.emit('setChannel', {'channelName': newChan});
+                } else {
+                    window.alert(`Channel name ${newChan} already in use.`)
+                }
+            } else {
+                window.alert("You must supply a channel name.")
+            }
         };
         
-        document.querySelector('#changeChannel').onclick = () => {
-                const newChannelName = document.querySelector('#changeChannel').value;
-                console.log(`set channel to: ${newChannelName}`);
-                localStorage.setItem("currChannel", newChannelName);
-                socket.emit('setChannel', {'channelName': newChannelName});
+        document.querySelector('#changeChannel').onchange = () => {
+            const newChannelName = document.querySelector('#changeChannel').value;
+            console.log(`set channel to: ${newChannelName}`);
+            localStorage.setItem("currChannel", newChannelName);
+            currChannel = newChannelName;
+            socket.emit('setChannel', {'channelName': newChannelName});
         };
         
         document.querySelector('#sendTextButton').onclick = () => {
-                let newText = document.querySelector('#sendText').value;
-                let newMsg = {"chan": currChannel,
-                              "text": newText, 
-                              "from": document.querySelector('#displayName').value
-                             }
-                console.log(`send message: ${newMsg}`);
-                socket.emit('sendMsg', newMsg);
+            let newText = document.querySelector('#sendText').value;
+            document.querySelector('#sendText').value = "";
+            enableSendButton();
+            let newMsg = {"chan": currChannel,
+                          "text": newText, 
+                          "from": document.querySelector('#displayName').value
+                         }
+            console.log(`send message: ${newMsg}`);
+            socket.emit('sendMsg', newMsg);
         };
 
     });
     
     // When we join the chat server, it sends us a list of current channels, and the last 100 msgs
     // on our current channel
-    // TODO: select current channel. . . 
-    // TODO: display messages for current channel
     socket.on('welcome', data => {
         console.log(`welcome received, data is ${data}`);
         for (let nxt = 0; nxt < data.length - 1; nxt++) {
             addSelectOptionUnique(document.querySelector("#changeChannel"), data[nxt]); 
         };
+        setSelectedChannel(document.querySelector("#changeChannel"), currChannel);
         msgList = data[data.length - 1];
-        let chatText = "";
-        for (let nxt = 0; nxt < msgList.length; nxt++) {
-                console.log("add line " + msgList[nxt]);
-                chatText += msgList[nxt]["text"];
-                chatText += "\n";
-        }
-        document.querySelector('#chatText').value = chatText;
+        setChatText(msgList);
 
     });
 
     // When a new message is received
     socket.on('new msg', data => {
-        // TODO - append to display if msg is part of current channel
-        console.log(data)
+        console.log(`date=${data}`)
         
-        if (data["chan"] === currChannel) {
+        if (data["chan"] == currChannel) {
             document.querySelector('#chatText').value += data["text"] + "\n";    
         }
     });
@@ -90,26 +99,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // name that the server no longer knows because it's been restarted.
     socket.on('channel added', data => {
         addSelectOptionUnique(document.querySelector("#changeChannel"), data); 
-        /*
-        let newChannel = document.createElement('option');
-        console.log(`add new channel ${data}`)
-        newChannel.textContent = data;
-        document.querySelector('#changeChannel').appendChild(newChannel);
-        */
+        setSelectedChannel(document.querySelector("#changeChannel"), currChannel);
     });
 
-    // TODO: common code to display list of messages, used for join and also chanaged channel
+    // when the channel is changed, the server sends the saved messages for the new channel
     socket.on('channel changed', data => {
-        let textArea = document.querySelector('#chatText');
-        console.log(`changed channel msg received: ${data}`);
-        //channels = {"Welcome!": [{"text":"Welcome to FlackChat!", "from":"admin", "time":""}, ]}
-        let chatText = "";
-        for (let nxt = 0; nxt < data.length; nxt++) {
-            console.log("add line " + data[nxt].text);
-            chatText += data[nxt].text;
-            chatText += "\n";
-        }
-        textArea.value = chatText;
+        console.log("changed channel msg received");
+        setChatText(data);
     });
     
     // Enable 'send' msg' button only if there is a display name present in the input field
@@ -119,26 +115,46 @@ document.addEventListener('DOMContentLoaded', () => {
         else
             document.querySelector('#sendTextButton').disabled = true;
     }
-
-    function enableNewChannelButton() {
-        if (document.querySelector('#newChannelName').value.length > 0)
-            document.querySelector('#newChannelButton').disabled = false;
-        else
-            document.querySelector('#newChannelButton').disabled = true;
-    }
     
     // will add newOption to selectControl if it isn't there already
     function addSelectOptionUnique(selectControl, newOption) {
-      let optionsList = selectControl.getElementsByTagName("option");
-      let currOptions = [];
-      for (let nxt = 0; nxt < optionsList.length; nxt++) {
-        currOptions.push(optionsList[nxt].textContent);
-      };
+      let currOptions = getChannelList(selectControl);
 
       if (!currOptions.includes(newOption)) {
          let newChannel = document.createElement('option');
          newChannel.textContent = newOption;
          selectControl.appendChild(newChannel);
       };
-    }        
+    }
+    
+    function getChannelList(selectControl) {
+        let optionsList = selectControl.getElementsByTagName("option");
+        let currOptions = [];
+        for (let nxt = 0; nxt < optionsList.length; nxt++) {
+            currOptions.push(optionsList[nxt].textContent);
+        }; 
+        return currOptions;
+    }
+    
+    function setSelectedChannel(selectControl, selectedItem) {
+        let optionsList = selectControl.getElementsByTagName("option");
+        for (var nxt = 0; nxt < optionsList.length; nxt++) {
+             if (optionsList[nxt].textContent == selectedItem) {
+                 optionsList[nxt].selected = true;
+             } else {
+                 optionsList[nxt].selected = false;
+             }
+        }
+    }
+    
+    function setChatText(msgList) {
+        let chatText = "";
+        for (let nxt = 0; nxt < msgList.length; nxt++) {
+            console.log("add line " + msgList[nxt].text);
+            chatText += msgList[nxt].text;
+            chatText += "\n";
+        }
+        document.querySelector('#chatText').value = chatText;
+        console.log("updated textArea.value")
+    }
 });
